6e93b1b5a9ad0d63e15d51ad219ccf7b46b5e584
Florian Pépin <pepinflorian627@gmail.com>
2025-02-08 16:07:07 +0100

    scripts

---
+++ dist/run-splitter.bat
@@ -0,0 +1,3 @@
+@echo off
+java -jar splitter.jar %1 %2 %3
+pause
--- src/splitter/Main.java
+++ src/splitter/Main.java
@@ -17,10 +17,18 @@ public class Main {
                 ImageCutter cutter = new ImageCutter(name, rows, columns);
                 cutter.cut();
             } catch (NumberFormatException e) {
-                System.err.println("Invalid parameter.");
+                System.err.println("Paramètres incorrects.");
             }
         } else {
-            System.out.println("No parameter provided.");
+            System.out.println("#####################################################################");
+            System.out.println("Paramètres incorrects.\n");
+            System.out.println("SUR LINUX:");
+            System.out.println("Usage: ./run-splitter.sh <image> <rows> <columns>\n");
+            System.out.println("SUR WINDOWS:");
+            System.out.println("Usage: run-splitter.bat <image> <rows> <columns>\n");
+            System.out.println("PAR DÉFAUT:");
+            System.out.println("Usage: java -jar splitter.jar <image> <rows> <columns>");
+            System.out.println("#####################################################################\n");
         }
     }
 }
